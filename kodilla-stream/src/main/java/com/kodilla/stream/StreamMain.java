ff7ce2b3196dd03e6c8fd12608932ab7072a798d
Pawel <pawel.stankiewicz95@gmail.com>
2021-03-01 14:07:02 +0100

    7.3 stream(), filter(), map(), collect()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,26 +1,25 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.time.LocalDate.now;
 
 public class StreamMain {
+
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theResultUserList = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getBirthdate().isBefore(now().minusYears(20)))
+                .filter(forumUser -> forumUser.getPostsNumber() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
 
-        poemBeautifier.beautify("poem", poem -> "ABC" + poem + "ABC");
-        poemBeautifier.beautify("poem", poem -> poem.toUpperCase());
-        PoemDecorator poemDotter = poem -> {
-            if ( !poem.endsWith(".") ) {
-                poem = poem + ".";
-            }
-            return  poem;
-        };
-        poemBeautifier.beautify("poem", poemDotter);
-        poemBeautifier.beautify("poem", poem -> "Start: " + poem);
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
+        theResultUserList.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
