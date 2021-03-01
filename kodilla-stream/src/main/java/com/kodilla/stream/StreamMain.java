8ceeb0d43e1e8f97c500e2d07774f91b6c671310
Pawel <pawel.stankiewicz95@gmail.com>
2021-03-01 13:04:37 +0100

    7.2 Immutable forum user

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -17,6 +18,9 @@ public class StreamMain {
         };
         poemBeautifier.beautify("poem", poemDotter);
         poemBeautifier.beautify("poem", poem -> "Start: " + poem);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
